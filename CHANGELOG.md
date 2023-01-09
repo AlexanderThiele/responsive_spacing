56837e59cfc5162c2cc7c3b2083d049e022f8b1b
alexanderthiele <alex@thiele.dev>
2023-01-09 10:01:50 +0100

    0.2.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.2.2
+* readme update. changed default specs
+
 ## 0.2.1
 * pubspec update, readme update
 
--- example/lib/responsive_collection/responsive_gutter.dart
+++ example/lib/responsive_collection/responsive_gutter.dart
@@ -2,23 +2,23 @@ import 'package:responsive_spacing/responsive_spacing.dart';
 
 class MyResponsiveGutters extends ResponsiveCollection {
   @override
-  ScaledSize fallback(double width) => const ScaledSize(size: 8);
+  ScaledSize xl(double width) => const ScaledSize(size: 16);
 
   @override
-  ScaledSize lg(double width) => const ScaledSize(size: 8);
+  ScaledSize lg(double width) => const ScaledSize(size: 16);
 
   @override
   ScaledSize md(double width) => const ScaledSize(size: 8);
 
   @override
-  ScaledSize sm1(double width) => const ScaledSize(size: 8);
+  ScaledSize sm2(double width) => const ScaledSize(size: 4);
 
   @override
-  ScaledSize sm2(double width) => const ScaledSize(size: 8);
+  ScaledSize sm1(double width) => const ScaledSize(size: 4);
 
   @override
-  ScaledSize xl(double width) => const ScaledSize(size: 8);
+  ScaledSize xs(double width) => const ScaledSize(size: 2);
 
   @override
-  ScaledSize xs(double width) => const ScaledSize(size: 8);
+  ScaledSize fallback(double width) => const ScaledSize(size: 2);
 }
